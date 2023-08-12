d61966c9f367723b8e1fc5587390af67f5cfd1da
Kobe Li <likobe23@gmail.com>
2023-08-11 21:25:23 -0700

    Add mongodb connection

---
+++ database/db.go
@@ -0,0 +1,29 @@
+package database
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func InitDB() *mongo.Client {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("You must set MONGODB_URI environment variable to your MongoDB connection string")
+	}
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+
+	return client
+}
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+
+	"go-todo/database"
 )
 
 func main() {
@@ -12,6 +15,14 @@ func main() {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
+
+	mongoClient := database.InitDB()
+
+	defer func() {
+		if err := mongoClient.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
 
 	log.Fatal(app.Listen(":3001"))
 }
